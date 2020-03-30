428aa77219eaf00bba0240a05f9d200472700067
Oleksandr Baior <43751093+OleksandrBaior@users.noreply.github.com>
2020-03-30 21:30:37 +0300

    Updates

---
+++ precache-manifest.4bdc5376184ea4a37a7de57bc917679d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7316ef3aa07086510524ee06f8819322",
+    "url": "/ReactMovies/index.html"
+  },
+  {
+    "revision": "6d36e802d18275693e9c",
+    "url": "/ReactMovies/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "310c4793f3e970618372",
+    "url": "/ReactMovies/static/css/main.06b23569.chunk.css"
+  },
+  {
+    "revision": "6d36e802d18275693e9c",
+    "url": "/ReactMovies/static/js/2.21700650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactMovies/static/js/2.21700650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "310c4793f3e970618372",
+    "url": "/ReactMovies/static/js/main.414865a8.chunk.js"
+  },
+  {
+    "revision": "eddf44c301c5fb438814",
+    "url": "/ReactMovies/static/js/runtime-main.b6f7bcb6.js"
+  }
+]);
